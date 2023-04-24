d594ed63e793824677babec8a5939b9c4ceffc1e
qyley <qyley@foxmail.com>
2023-04-24 15:45:23 +0800

    test

---
+++ source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Limber FPGA Platform'
+copyright = '2023, QyLey'
+author = 'QyLey'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'zh_CN'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
+html_static_path = [sphinx_rtd_theme.get_html_theme_path()]
---
+++ source/index.rst
@@ -0,0 +1,20 @@
+.. Limber FPGA Platform documentation master file, created by
+   sphinx-quickstart on Mon Apr 24 15:15:23 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Limber FPGA Platform Cookbook
+================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   hello
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
